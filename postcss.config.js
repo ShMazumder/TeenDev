4e723b5fd347fc893c59c4afb2bddc797e5dbc61
shmazumder23 <shmazumder23@gmail.com>
2025-04-26 19:48:01 +0000

    up

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,12 @@
 // postcss.config.js
 module.exports = {
-    plugins: {
-      'postcss-import': {}, // For importing other CSS files
-      '@tailwindcss/nesting': {}, // Enable nested CSS (like Sass)
-      tailwindcss: {}, // Tailwind CSS
-      autoprefixer: {}, // Auto-prefixer for vendor prefixes
-      ...(process.env.NODE_ENV === 'production' ? {
-        cssnano: { // Minify CSS only in production
-          preset: 'default',
-        }
-      } : {})
-    }
+  plugins: {
+    tailwindcss: {}, // Tailwind CSS
+    autoprefixer: {}, // Auto-prefixer for vendor prefixes
+    ...(process.env.NODE_ENV === 'production' ? {
+      cssnano: { // Minify CSS only in production
+        preset: 'default',
+      }
+    } : {})
   }
+}
